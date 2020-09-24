c3c44c2992e4f6aa3631b62e190babd7f604f94b
jcmcarth06 <jcmcarth06@gmail.com>
2020-09-23 18:30:17 -0600

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Puzzle.create(question: 'Gloves for baby cats', solution: 'kittens mittens')
+Puzzle.create(question: 'A filthy, small flying animal', solution: 'dirty birdy')
+Puzzle.create(question: 'A rude Scottish girl', solution: 'sassy lassie')
+Puzzle.create(question: 'Worthless coins', solution: 'funny money')
+Puzzle.create(question: 'A happy round juicy grape', solution: 'merry berry')
+Puzzle.create(question: 'A soft shaky stomach', solution: 'jelly belly')
+Puzzle.create(question: 'Goofy William', solution: 'silly Billy')
+Puzzle.create(question: 'A scary bright flash you see during a storm', solution: 'frightening lightning')
+Puzzle.create(question: 'A happy flying elf', solution: 'merry fairy')
